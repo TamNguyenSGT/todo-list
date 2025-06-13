fc3aa6b11d3e2bc41b509e70a0daf573a3fee809
nttam.sgt <thanhtam1011.sgt@gmail.com>
2025-06-13 11:08:20 +0700

    new updated

---
+++ todo-app/backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const todoRoutes = require('./routes/todos');
+app.use('/api/todos', todoRoutes);
+
+app.listen(3002, () => console.log('🚀 Server running on http://localhost:3002'));
