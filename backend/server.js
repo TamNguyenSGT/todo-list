067ebf26bc26b8e6265264dc6ccc9030bf6c08d7
nttam.sgt <thanhtam1011.sgt@gmail.com>
2025-06-13 10:05:03 +0700

    coding process (server.js)

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const todoRoutes = require('./routes/todos');
+app.use('/api/todos', todoRoutes);
+
+app.listen(5000, () => console.log('Server running on port 5000'));
