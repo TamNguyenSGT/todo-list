80dc3561315b4446f75d65e5b1ee410a40da0a88
nttam.sgt <thanhtam1011.sgt@gmail.com>
2025-06-25 16:34:50 +0700

    #33 feat: changes port

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ dotenv.config();
 const db = require("./config/db");
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 
 app.use(cors());
 app.use(express.json());
